f0a75da9faaec8c1a473658938053a628752f949
luis arthur <luisarthurportelanassar@gmail.com>
2025-02-19 22:05:09 -0300

    primeiro commit

--- aula2.py
+++ aula2.py
@@ -1,1 +1,12 @@
-print("professor,é com muito pesar que eu suplico para o senhor adicionar 1,0 ponto na minha nota em sua materia")
+def funcao():
+    #print()
+    print("professor,é com muito pesar que eu suplico para o senhor adicionar 1,0 ponto na minha nota em sua materia")
+
+funcao()
+funcao()
+funcao()
+funcao()
+funcao()
+funcao()
+funcao()
+funcao()
