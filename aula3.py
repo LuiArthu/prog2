a2138c10d33351721a3d63205882f2c4edcbfb2e
luis arthur <luisarthurportelanassar@gmail.com>
2025-02-25 19:55:08 -0300

    primeiro commit

---
+++ aula3.py
@@ -0,0 +1,19 @@
+#def funcao():
+#    a = 5
+#    b = 7
+#    print(a + b)
+
+#funcao()
+
+def somar(a, b):
+    (a + b)
+
+def dividir(a, b):
+    (a / b)
+
+def multiplicar(a, b):
+    (a * b)
+
+def 
+
+    
