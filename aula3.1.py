53f29cd64254c12421560d36ca057a7ec69d37cf
luis arthur <luisarthurportelanassar@gmail.com>
2025-02-25 21:42:15 -0300

    calculadora

---
+++ aula3.1.py
@@ -0,0 +1,38 @@
+#def funcao():
+#    a = 5
+#    b = 7
+#    print(a + b)
+
+#funcao()
+
+
+
+def dividir(a, b):
+    if (b == 0):
+        print("divisão por 0")
+    else:
+        print(f'o resultado da divisão entre {a} e {b} é : {a / b}')
+
+def multiplicar(a, b):
+    (a * b)
+    print(f'o resultado da multiplicação entre {a} e {b} é : {a * b}')
+
+def subitrair(a, b):
+    (a - b)
+
+def somar(a, b):
+    (a + b)
+
+var1 = input("digite um numero:")
+#cin<<var1;
+
+if (var1 == '0'):
+    print("olá")
+
+multiplicar(var1, 8)
+
+var2 = int(input("digite um numero:"))
+var3 = int(input("digite um numero:"))
+
+multiplicar(var2, var3)
+    
---
+++ aula3.2.py
@@ -0,0 +1,28 @@
+print("-----------------calculadora-----------------")
+num1 = int(input("digite um numero:"))
+print("---------------------------------------------")
+
+
+print("---------------------------------------------")
+print("digite .+. para uma operação de soma")
+print("digite .-. para uma operação de subitração")
+print("digite .x. para uma operação de multiplicação")
+print("digite .:. para uma operação de divisão")
+print("---------------------------------------------")
+selecionador = input(f"escolha o tipo de operação que dejesa:")
+print("---------------------------------------------")
+num2 = int(input("digite o segundo numero numero:"))
+print("---------------------------------------------")
+
+
+if (selecionador == '+'):
+    print(f"{num1} + {num2} = {num1 + num2}")
+else:
+    if (selecionador == '-'):
+        print(f"{num1} - {num2} = {num1 - num2}")
+    else:
+        if (selecionador == 'X'):
+            print(f"{num1} X {num2} = {num1 * num2}")
+        else:
+            if (selecionador == ':'):
+                print(f"{num1} : {num2} = {num1 / num2}")
