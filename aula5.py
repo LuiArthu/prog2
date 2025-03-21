67476330807cae0728466c9a95bcde6680dbff1c
Luis Arthur <luisarthurportelanassar@gmail.com>
2025-03-21 20:15:11 -0300

    fibonacci

---
+++ aula5.py
@@ -0,0 +1,13 @@
+
+def Fibonacci(ESCOLHA):
+    if ESCOLHA == 0:
+        return 1
+    else:
+        firesultado = (ESCOLHA - 1) + (ESCOLHA - 2)
+        
+Fibonacci(4)
+print(resultado)
+
+
+
+    
