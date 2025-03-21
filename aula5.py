d00c72c555b4ddf20d58640076377a72739294c7
Luis Arthur <luisarthurportelanassar@gmail.com>
2025-03-21 20:17:25 -0300

    fibonacci

--- aula5.py
+++ aula5.py
@@ -1,13 +1,15 @@
-
-def Fibonacci(ESCOLHA):
-    if ESCOLHA == 0:
+def Fibonacci (num):
+    if num == 1:
+        return 1
+    elif num == 2:
         return 1
     else:
-        firesultado = (ESCOLHA - 1) + (ESCOLHA - 2)
-        
-Fibonacci(4)
-print(resultado)
-
+        return Fibonacci (num - 1) + Fibonacci (num - 2)
+numero = 1
+entrada = int(input("Numera para calcular o fibonacci:"))
+while numero <= entrada:
+    print(f"fibonacci de {numero}:{Fibonacci(numero)}")
+    numero = numero + 1
 
 
     
