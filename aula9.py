2bc4e802ec477178e33ec81935711cc029eb718e
luis arthur <luisarthurportelanassar@gmail.com>
2025-05-23 21:04:02 -0300

    aula9

---
+++ aula9.py
@@ -0,0 +1,33 @@
+class vo():
+    def __init__(self, planta):
+        self.planta = planta
+        
+    def exibe_planta (self):
+        print(self.planta)
+
+class pai (vo):
+    def __init__(self, planta, planta2):
+        super().__init__(planta)
+        self.planta2 = planta2
+    
+    def exibe_planta (self):
+        print(f"{self.planta} e {self.planta2}")
+
+class eu (pai):
+    def __init__(self, planta, planta2, planta3):
+        super().__init__(planta, planta2)
+        self.planta3 = planta3
+
+    def exibe_planta (self):
+        super().exibe_planta(f" e {self.planta3}")
+
+vo = vo("batata")
+pai = pai("batata","uva")
+eu = eu("batata","uva", "maca")
+
+print('-------vo------')
+vo.exibe_planta()
+print('------pai------')
+pai.exibe_planta()
+print('-----filho-----')
+eu.exibe_planta()
